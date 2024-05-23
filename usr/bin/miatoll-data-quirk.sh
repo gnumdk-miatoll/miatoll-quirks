45347733a68ed967c685db95625a92f7413b7ed9
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-05-23 15:37:17 +0200

    Fix data quirk script

--- usr/bin/miatoll-data-quirk.sh
+++ usr/bin/miatoll-data-quirk.sh
@@ -2,7 +2,8 @@
 ping -c1 8.8.8.8 -W 5 >/dev/null 2>&1
 if (( $? != 0 ))
 then
-	nmcli c down Orange
+    provider=$(nmcli c | grep gsm | awk '{ print $1 }')
+	nmcli c down $provider
 	sleep 1
-	nmcli c up Orange
+	nmcli c up $provider
 fi
