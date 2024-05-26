37e9c06cad7eb76ce72261f6bc2d0fbf76464a81
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-05-26 23:09:58 +0200

    miatoll-quirks.sh: Use sh

--- usr/bin/miatoll-quirks.sh
+++ usr/bin/miatoll-quirks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Camera does not work after a reboot, like if camera ids are changing
 sed -i '/camera.*/d' ~/.config/Droidian/droidian-camera.conf
 # Set something else than 100% volume
