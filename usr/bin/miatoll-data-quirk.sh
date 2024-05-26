01169ebc489b4c12813a303d3e7b6ee6102d4c5a
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-05-26 22:52:45 +0200

    network: Rework the way we handle modem quirk

--- usr/bin/miatoll-data-quirk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ping -c1 8.8.8.8 -W 5 >/dev/null 2>&1
-if (( $? != 0 ))
-then
-    provider=$(nmcli c | grep gsm | awk '{ print $1 }')
-	nmcli c down $provider
-	sleep 1
-	nmcli c up $provider
-fi
---
+++ usr/lib/NetworkManager/dispatcher.d/99-miatoll.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DEVICE=$1
+STATUS=$2
+
+[ "$DEVICE" != "/ril_0" ] && exit 0
+[ "$STATUS" != "down" ] && exit 0
+if grep "WWANEnabled=true" /var/lib/NetworkManager/NetworkManager.state >/dev/null
+then
+    enabled=$(nmcli r wwan)
+    [ "$enabled" = "enabled" ] && nmcli d connect $DEVICE
+fi
