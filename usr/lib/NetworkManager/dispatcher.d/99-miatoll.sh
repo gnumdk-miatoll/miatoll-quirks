20b124a20beabf0c8f3b505f858fb6bbd3752dca
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-05-27 00:43:37 +0200

    nm: Better fix to come

--- usr/lib/NetworkManager/dispatcher.d/99-miatoll.sh
+++ usr/lib/NetworkManager/dispatcher.d/99-miatoll.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DEVICE=$1
 STATUS=$2
-
+exit 0
 [ "$DEVICE" != "/ril_0" ] && exit 0
 [ "$STATUS" != "down" ] && exit 0
 if grep "WWANEnabled=true" /var/lib/NetworkManager/NetworkManager.state >/dev/null
