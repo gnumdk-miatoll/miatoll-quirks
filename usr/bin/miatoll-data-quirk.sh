8f9a119559f31bb6a3c830122140ddadc5787a38
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-02 00:54:12 +0200

    miatoll-data-quirk: Use user provider

--- usr/bin/miatoll-data-quirk.sh
+++ usr/bin/miatoll-data-quirk.sh
@@ -4,7 +4,7 @@ ip route ls | grep rmnet | grep 'default via' >/dev/null
 if (( $? != 0 ))
 then
     provider=$(nmcli c | grep gsm | awk '{ print $1 }')
-    LANG=C nmcli connection show Orange | grep connection.autoconnect: | grep yes >/dev/null || exit 0
+    LANG=C nmcli connection show $provider | grep connection.autoconnect: | grep yes >/dev/null || exit 0
     nmcli c down $provider
     sleep 1
     nmcli c up $provider
