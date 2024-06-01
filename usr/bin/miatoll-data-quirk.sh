169a19c568b7dfb0e8672f2b54b482aa2992e2ac
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-02 00:37:29 +0200

    miatoll-data-quirk: system timer

--- usr/bin/miatoll-data-quirk.sh
+++ usr/bin/miatoll-data-quirk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ip route ls | grep rmnet >/dev/null || exit 0
-
+grep 'WWANEnabled=true' /var/lib/NetworkManager/NetworkManager.state >/dev/null || exit 0
 ip route ls | grep rmnet | grep 'default via' >/dev/null
 if (( $? != 0 ))
 then
     provider=$(nmcli c | grep gsm | awk '{ print $1 }')
-	nmcli c down $provider
-	sleep 1
-	nmcli c up $provider
+    LANG=C nmcli connection show Orange | grep connection.autoconnect: | grep yes >/dev/null || exit 0
+    nmcli c down $provider
+    sleep 1
+    nmcli c up $provider
 fi
