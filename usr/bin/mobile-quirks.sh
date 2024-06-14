7a62f32f57283d397d8dbda4f65e678950a9ed0c
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-14 16:24:01 +0200

    mobile-quirks.sh: Set volume to 90%

--- usr/bin/mobile-quirks.sh
+++ usr/bin/mobile-quirks.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# Camera does not work after a reboot, like if camera ids are changing
-sed -i '/camera.*/d' ~/.config/Droidian/droidian-camera.conf
 # Set something else than 100% volume
-pactl set-sink-volume 0 65%
+pactl set-sink-volume 0 90%
