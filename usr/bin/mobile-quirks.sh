d6e4afee64c94c77c08940c3a8156b3909cfd0ab
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-07-03 16:29:53 +0200

    quirks: Clean up camera app configuration on logon

--- usr/bin/mobile-quirks.sh
+++ usr/bin/mobile-quirks.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Set something else than 100% volume
 pactl set-sink-volume 0 90%
+# Camera does not work after a reboot, like if camera ids are changing
+sed -i '/camera.*/d' ~/.config/Droidian/droidian-camera.conf
