deed9b48ef7cc13ad56e93064991afc264c23f2b
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-07-10 07:32:53 +0200

    mobile-data-quirks.sh: Use UUID to match provider

--- usr/bin/mobile-data-quirk.sh
+++ usr/bin/mobile-data-quirk.sh
@@ -3,9 +3,9 @@ grep 'WWANEnabled=true' /var/lib/NetworkManager/NetworkManager.state >/dev/null 
 ip route ls | grep rmnet | grep 'default via' >/dev/null
 if (( $? != 0 ))
 then
-    provider=$(nmcli c | grep gsm | awk '{ print $1 }')
-    LANG=C nmcli connection show $provider | grep connection.autoconnect: | grep yes >/dev/null || exit 0
-    nmcli c down $provider
+    provider="$(nmcli c show | grep gsm | grep -oE '[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*')"
+    LANG=C nmcli connection show "$provider" | grep connection.autoconnect: | grep yes >/dev/null || exit 0
+    nmcli c down "$provider"
     sleep 1
-    nmcli c up $provider
+    nmcli c up "$provider"
 fi
